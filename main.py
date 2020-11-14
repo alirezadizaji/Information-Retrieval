301396ad777d282e03b64390763acb406795fef6
Sabrineh <Sabrinehmokhtari@yahoo.com>
2020-11-14 14:36:25 +0330

    # Main Updated

--- main.py
+++ main.py
@@ -13,13 +13,13 @@ if __name__ == '__main__':
         eng_total_documents = 0
         while True:
             print(" Choose a number:\n",
-                  "1-Create positional index\n",
+                  "1- Create positional index\n",
                   "2- Save positional index\n",
                   "3- Get posting list\n",
                   "4- Create bigram\n",
                   "5- Show bigram\n",
                   "6- Lnc-ltc\n",
-                  "7-Quit")
+                  "7- Quit")
             cmd = input()
             if cmd == '1':
                 eng_title_posting_list, eng_document_posting_list, eng_total_documents = eng_create_index(
@@ -57,13 +57,13 @@ if __name__ == '__main__':
         wiki_total_documents = 0
         while True:
             print(" Choose a number:\n",
-                  "1-Create positional index\n",
+                  "1- Create positional index\n",
                   "2- Save positional index\n",
                   "3- Get posting list\n",
                   "4- Create bigram\n",
                   "5- Show bigram\n",
                   "6- Lnc-ltc\n",
-                  "7-Quit")
+                  "7- Quit")
             cmd = input()
             if cmd == '1':
                 wiki_title_posting_list, wiki_document_posting_list, wiki_total_documents = wiki_create_index(
